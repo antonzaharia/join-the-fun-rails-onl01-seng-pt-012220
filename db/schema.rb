e4cfaac07cee4ef5f93b21f9be81fa3ccec31455
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-05 16:32:09 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200605162733) do
-
-  create_table "passenger_taxis", force: :cascade do |t|
-    t.integer  "passenger_id"
-    t.integer  "taxi_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
+ActiveRecord::Schema.define(version: 20200605161736) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
